dd5e818fa6d77462610e74d59edfddf1369c299d
Kurt Milam <kurt.milam@xioup.com>
2017-07-28 22:48:27 +0200

    Create dispatchMap in one go (got rid of allCasesFns)

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import * as R   from "ramda"
+import * as R_  from "./ramda"
 import { env }  from "sanctuary"
 import $        from "sanctuary-def"
 import type     from "sanctuary-type-identifiers"
@@ -8,11 +9,11 @@ import type     from "sanctuary-type-identifiers"
 const log = R.tap( console.log )
 
 const memo = R.memoize
-const mergeDL = R.mergeDeepLeft
 const nTest = $.test( [] )
 const o = f => g => h => f( g( h ) )
-const containedBy = R.flip( R.contains )
-
+const contains_ = R.flip( R.contains )
+const concat_ = R.flip( R.concat )
+const W = f => x => f( x )( x )
 
 export const createSumTypeFactory = opts => {
   const def = $.create( opts )
@@ -49,172 +50,167 @@ export const createSumTypeFactory = opts => {
                    , x => false
                    )
 
+    // _allCasesMap :: StrMap( Case )
+    // StrMap of cases
+    const _allCasesMap =
+      W( o( R.zipObj )
+          ( R.pluck( 'tag' ) )
+       )
+       ( cases )
+
     // _allCasesTags :: Array( String )
     // Array with one _tag per case
     const _allCasesTags =
-      R.pluck( 'tag' )( cases )
+      R.keys( _allCasesMap )
 
-    // _allCasesTypes :: Array( Type )
-    // Array with one Type per case
-    const _allCasesTypes =
-      R.map( ( { tag, type: t } ) =>
-               $.NullaryType( `${ nameSpace }/${ name }.${ tag }`
-                            , url
-                            , nTest( $.Type, t )
-                                // We have a type - wrap and return it
-                                ? x =>
-                                    type( x ) === typeIdentifier
-                                      ? nTest( t, x.value )
-                                      : nTest( t, x )
-                                : R.is( Function, t )
-                                  // createSumTypeFactory Type from predicate function
-                                  ? x =>
-                                      type( x ) === typeIdentifier
-                                        ? t( x.value )
-                                        : t( x )
-                                  // createSumTypeFactory Unit Type
-                                  : x =>
-                                      type( x ) === typeIdentifier
-                                        ? t === x.value
-                                        : t === x
-                        )
+    // _allCasesTypesMap :: Array( Type )
+    // StrMap with one Type per case
+    const _allCasesTypesMap =
+      R.reduce( ( acc, { tag, type: t } ) =>
+                  R.assoc( tag )
+                         ( $.NullaryType( `${ nameSpace }/${ name }.${ tag }`
+                                        , url
+                                        , nTest( $.Type, t )
+                                            // We have a type - wrap and return it
+                                            ? x =>
+                                                type( x ) === typeIdentifier
+                                                  ? nTest( t, x.value )
+                                                 : nTest( t, x )
+                                            : R.is( Function, t )
+                                              // createSumTypeFactory Type from predicate function
+                                              ? x =>
+                                                 type( x ) === typeIdentifier
+                                                    ? t( x.value )
+                                                   : t( x )
+                                              // createSumTypeFactory Unit Type
+                                              : x =>
+                                                  type( x ) === typeIdentifier
+                                                    ? t === x.value
+                                                    : t === x
+                                        )
+                         )
+                         ( acc )
            )
+           ( {} )
            ( cases )
 
-    // _allCasesMap :: StrMap( Case )
-    // Object map of cases
-    const _allCasesMap =
-      R.zipObj( _allCasesTags )( cases )
-
     // _allTypesMap :: StrMap( Type )
-    // StrMap with one Type per case + the SumType
+    // StrMap with one Type per case + the SumType & Placeholder
     const _allTypesMap =
-      R.zipObj( R.concat( [ 'ST', 'PT' ]
-                        , _allCasesTags
-                        )
-              )
-              ( R.concat( [ SumTypeType, PlaceholderType ]
-                        , _allCasesTypes
-                        )
-              )
-
-    // _allTypes :: Array( Type )
-    // Array with one Type per case + the SumType & Placeholder
-    const _allTypes =
-      R.values( _allTypesMap )
+      R.merge( _allCasesTypesMap )
+             ( { ST: SumTypeType
+               , PT: PlaceholderType
+               }
+             )
 
     // _allFnNames :: Array( Case ) -> Array( String )
-    // Unique array of all case and default function names
+    // Unique array of all function names
     const _allFnNames =
-      R.pipe( R.pluck( 'fns' )
-            , R.ap( [ R.keys ] )
+      R_.p( [ R.pluck( 'fns' )
+            , R.map( R.keys )
+            , R.values
             , R.flatten
             , R.uniq
-            )
-            ( cases )
+            ]
+          )
+          ( _allCasesMap )
+
+    /*
+    * Function definition Begins here
+    */
+
+    const isConstructed =
+        x => type( x ) === typeIdentifier
 
     const _throwMissingFunctionErr =
       ( fnName, tag ) =>
         { throw new TypeError( `No '${ fnName }' function defined on case '${ tag }'.` ) }
 
-    // _allCasesFns :: Array( Case ) -> Array( Fn )
-    const _allCasesFns =
-      R.reduce( ( acc, fnName ) =>
-                mergeDL( R.reduce( ( acc2, { tag, fns } ) =>
-                                     R.ifElse( R.is( Function )
-                                     , x =>
-                                         R.assocPath( [ tag, fnName ], x, acc2 )
-                                     , _ =>
-                                         _throwMissingFunctionErr( fnName, tag )
-                                     )
-                                     ( fns[ fnName ] )
-                                 )
-                                 ( {} )
-                                 ( cases )
-                       )
-                       ( acc )
-            )
-            ( {} )
-            ( _allFnNames )
+    const _dispatchMap =
+      ( fnName, sig ) =>
+        R.fromPairs(
+          R.map( tag =>
+                   { const fn =
+                       R.path( [ tag, 'fns', fnName ] )
+                             ( _allCasesMap )
+                     return(
+                       R.is( Function, fn )
+                         ? [ tag
+                           , def( fnName )
+                                ( {} )
+                                ( R.map( R.when( R.equals( PlaceholderType ) )
+                                               ( _ => _allCasesTypesMap[ tag ] )
+                                       )
+                                       ( sig )
+                                )
+                                ( fn )
+                           ]
+                         : _throwMissingFunctionErr( fnName, tag )
+                     )
+                   }
+               )
+               ( _allCasesTags )
+        )
 
-    /*
-    * Function definition Begins here
-    */
+    const _throwMissingSignatureErr =
+      ( fnName, name ) =>
+        { throw new TypeError( `Missing or invalid signature for function '${ fnName }' on '${ name }'.` ) }
 
     const _dispatchFn =
       fnName =>
         { const sigFn = R.prop( fnName, fnSigs )
           const sig = sigFn( _allTypesMap )
           if ( !nTest( $.Array( $.Type ), sig ) )
-            { throw new TypeError( `Missing or invalid signature for function '${ fnName }' on '${ name }'.` ) }
-          // Get the index of the last __input__ that is of our SumTypeType. This is used to determine
+            _throwMissingSignatureErr( fnName, name )
+          // Get the index of the last __input__ that is of our SumType. This is used to determine
           // whether or not to return a constructed value when the return value is toName our SumTypeType.
           const typeArgIndex =
-            R.findLastIndex( containedBy( _allTypes ), R.init( sig ) )
-          // If we return a value of our SumTypeType and the last __input__ of our SumTypeType is constructed,
+            R.findLastIndex( contains_( R.values( _allTypesMap ) ), R.init( sig ) )
+          // If we return a value of our SumType and the last __input__ of our SumType is constructed,
           // we return a constructed value, otherwise a bare value.
           const returnsOurType =
-            R.contains( R.last( sig ), _allTypes )
+            R.contains( R.last( sig ), R.values( _allTypesMap ) )
+          const dispatchMap =
+            _dispatchMap( fnName, sig )
+
           return (
-            R.curryN( _staticMethodArity( fnName )
-            , ( ...args ) =>
-               { const x = args[ typeArgIndex ]
-                 const kase = _firstMatchingCase( x )
-                 const typeArgIsConstructed =
-                   type( x ) === typeIdentifier
-                 const tag =
-                   typeArgIsConstructed
-                     ? x.tag
-                     : kase.tag
-                 const fn =
-                   checkTypes
-                     ? def( fnName )
-                          ( {} )
-                          ( R.map( R.when( R.equals( PlaceholderType ) )
-                                         ( _ => _allTypesMap[ tag ] )
-                                 )
-                                 ( sig )
-                          )
-                          ( R.path( [ tag, fnName ] )
-                                  ( _allCasesFns )
-                          )
-                     : R.path( [ tag, fnName ] )
-                             ( _allCasesFns )
-                 const bareRes =
-                   fn( ...R.map( _value )( args ) )
-                 return(
-                   typeArgIsConstructed && returnsOurType
-                     ? tagIt( bareRes, kase )
-                     : bareRes
-                 )
-               }
-            )
+            R.curryN( _staticMethodArity( fnName ) )
+                    ( ( ...args ) =>
+                       { const x = args[ typeArgIndex ]
+                         const kase = _firstMatchingCase( x )
+                         const typeArgIsConstructed =
+                           isConstructed( x )
+                         const tag =
+                           typeArgIsConstructed
+                             ? x.tag
+                             : kase.tag
+                         const bareRes =
+                           dispatchMap[ tag ]( ...R.map( _value )( args ) )
+                         return(
+                           typeArgIsConstructed && returnsOurType
+                             ? tagIt( bareRes, kase )
+                             : bareRes
+                         )
+                       }
+                    )
           )
         }
 
     const _staticMethodArity =
-      fnName =>
-        { const sigFn = R.prop( fnName, fnSigs )
-          const sig = sigFn( _allTypesMap )
-          return sig.length - 1
-        }
+      fnName => R.prop( fnName, fnSigs )( _allTypesMap ).length - 1
 
     const _instanceMethodArity =
-      fnName =>
-        { const sigFn = R.prop( fnName, fnSigs )
-          const sig = sigFn( _allTypesMap )
-          return sig.length - 2
-        }
+      fnName => _staticMethodArity( fnName ) - 1
 
-    // _sharedFns :: Array( Case ) -> Pair( String, Fn )
+    // _sharedFns :: Tuple( String, Fn, $.NonNegativeInteger )
     const _sharedFns =
-      R.map( R.converge( ( ...xs ) => xs )
-                       ( [ x => x, _dispatchFn, _instanceMethodArity ] )
+      R.map( o( R.ap( [ x => x, _dispatchFn, _instanceMethodArity ] ) )
+              ( R.of )
            )
            ( _allFnNames )
 
     const _value =
-      R.ifElse( x => type( x ) === typeIdentifier )
+      R.ifElse( isConstructed )
               ( x => x.value )
               ( x => x )
 
@@ -223,8 +219,8 @@ export const createSumTypeFactory = opts => {
     const tags =
       x =>
         R.reduce( ( acc, { tag } ) =>
-                  ( type( x ) === typeIdentifier && x[ 'is' + tag ] )
-                  || nTest( _allTypesMap[ tag ], x )
+                  ( isConstructed( x ) && x[ 'is' + tag ] )
+                  || nTest( _allCasesTypesMap[ tag ], x )
                     ? R.append( tag, acc )
                     : acc
                 )
@@ -233,24 +229,27 @@ export const createSumTypeFactory = opts => {
 
     // This lets us memoize calls to the tags 'instance' method
     const _dispatchTags =
-      R.ifElse( x => type( x ) === typeIdentifier )
+      R.ifElse( isConstructed )
               ( x => x.tags() )
               ( tags )
 
     // tagIt :: Any -> Case -> SumType
     const _tagIt =
       ( x, { tag } ) =>
-        { let r = {}
-          const tmp =
+        { const value =
+            checkTypes && R.is( Object )
+              ? Object.freeze( Object.assign( {}, x ) )
+              : x
+          const r =
             { name
             , url
-            , '@@type':    typeIdentifier
-            , constructor: SumTypeTypeRep
-            , value:       Object.assign( {}, x )
+            , value
             , tag
-            , tags:        R.once( _ => tags( r ) )
-            , is:          memo( tag => is( tag, r ) )
-            , hasTags:     memo( tagNames => hasTags( tagNames, r ) )
+            , '@@type':       typeIdentifier
+            , constructor:    SumTypeTypeRep
+            , tags:           R.once( _ => tags( r ) )
+            , is:             memo( tag => is( tag, r ) )
+            , hasTags:        memo( tagNames => hasTags( tagNames, r ) )
             , [ 'is' + tag ]: true
             }
 
@@ -258,24 +257,24 @@ export const createSumTypeFactory = opts => {
           // Mutation ahead!
           _sharedFns.forEach(
             ( [ name, fn, arity ] ) =>
-              arity < 2
-                ? r[ name ] = memo( ( ...ys ) => fn( ...R.append( r, ys ) ) )
-                : r[ name ] =
-                    R.curryN( arity
+              r[ name ] =
+                arity < 2
+                  ? memo( ( ...ys ) => fn( ...R.append( r, ys ) ) )
+                  : R.curryN( arity
                             , memo( ( ...ys ) => fn( ...R.append( r, ys ) ) )
                             )
           )
-          return Object.assign( r, tmp )
+          return r
         }
     const tagIt = def( 'tagIt', {}, [ SumTypeType, $.Object, SumTypeType ], _tagIt )
 
     // _firstMatchingCase :: Any -> Object
     const _firstMatchingCase =
       x =>
-        type( x ) === typeIdentifier
+        isConstructed( x )
           ? _allCasesMap[ x.tag ]
           : R.reduce( ( _, kase ) =>
-                        nTest( _allTypesMap[ kase.tag ] )
+                        nTest( _allCasesTypesMap[ kase.tag ] )
                              ( x )
                            ? R.reduced( kase )
                            : false
@@ -290,7 +289,7 @@ export const createSumTypeFactory = opts => {
 
     // _getTag :: Any -> string
     const _getTag =
-      R.ifElse( x => type( x ) === typeIdentifier )
+      R.ifElse( isConstructed )
               ( x => x.tag )
               ( o( R.prop( 'tag' ) )
                  ( _firstMatchingCase )
@@ -304,8 +303,8 @@ export const createSumTypeFactory = opts => {
     const is =
       ( tag, x ) =>
         R.contains( tag, _allCasesTags )
-          ? ( type( x ) === typeIdentifier && x[ 'is' + tag ] )
-            || nTest( _allTypesMap[ tag ], x )
+          ? ( isConstructed( x ) && x[ 'is' + tag ] )
+            || nTest( _allCasesTypesMap[ tag ], x )
           : _throwInvalidTagErr( tag, name )
 
     // hasTags :: String -> SumType -> Either( ? )
@@ -330,17 +329,18 @@ export const createSumTypeFactory = opts => {
            // caseFns :: StrMap( Function )
       , ...R.fromPairs( _sharedFns )
            // caseTypes :: StrMap( Type )
-      , ...R.pipe( R.converge( R.zip )
-                             ( [ _allCasesTags, _allCasesTypes ] )
-                 , R.map( ( [ tag, type ] ) => [ tag + 'Type', type ] )
+      , ...R_.p( [ R.toPairs
+                 , R.map( R.over( R.lensIndex( 0 )
+                                , concat_( 'Type' )
+                                )
+                        )
                  , R.fromPairs
-                 )
+                 ]
+               )
+               ( _allCasesTypesMap )
            // caseConstructors :: StrMap( Function )
-      , ...R.fromPairs( R.map( R.converge( R.pair )
-                                         ( [ R.prop( 'tag' ), _ => _toFirstMatch ] )
-                             )
-                             ( cases )
-                      )
+      , ...R.map( _ => _toFirstMatch )
+                ( _allCasesMap )
       }
     )
   }
@@ -358,51 +358,3 @@ export const createSumTypeFactory = opts => {
 // import { env }  from "sanctuary"
 // import { createSumTypeFactory  }  from './union-type'
 // const SumType = createSumTypeFactory( { checkTypes: true, env } )
-
-
-//region Tuple and UntaggedSumType
-const isOfTypeOrEqual =
-    t => x =>
-      ( nTest( $.Type, t ) && nTest( t, x ) === true )
-      || ( R.is( Function, t ) && t( x ) === true )
-      || t === x
-
-const UntaggedSumType =
-  ( name, url, cases ) =>
-    x =>
-      R.reduce( ( _, kase ) =>
-                  isOfTypeOrEqual( kase )( x ) === true
-                    ? R.reduced( x )
-                    : S.Left( 'No match' )
-              , false
-              , cases
-              )
-
-const validateTupleEntries =
-  R.reduce( ( _, [ x, type ] ) =>
-              isOfTypeOrEqual( type )( x ) === true
-              || R.reduced( false )
-          , false
-          )
-
-const validateTuple =
-  xs => types =>
-    R.when( ts => ts.length === xs.length
-          , o( validateTupleEntries )
-             ( R.zip( xs ) )
-          )
-          ( types )
-
-const TupleType =
-  ( name, url, types ) =>
-      $.NullaryType( name
-                   , url
-                   , ( ...x ) =>
-                       validateTuple( x )( types ) === true
-                   )
-const Point1 =
-  TupleType( 'Point'
-           , 'url'
-           , [ $.ValidNumber, $.ValidNumber ]
-           )
-//endregion
