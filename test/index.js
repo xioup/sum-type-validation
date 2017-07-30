662d88909c3f7d2dc81a4536f39d766b871987e8
Kurt Milam <kurt.milam@xioup.com>
2017-07-31 00:09:31 +0200

    Start setting up tests in mocha

--- test/index.js
+++ test/index.js
@@ -1,3 +1,40 @@
+import { createSumTypeFactory } from "../src/index"
+import R                        from "ramda"
+
+const { env } = require( "sanctuary" )
+const $ =       require( "sanctuary-def" )
+const type =    require( "sanctuary-type-identifiers" )
+const assert =  require( "assert" )
+
+const log = R.tap( console.log )
+const opts = { checkTypes:true, env }
+const def = $.create( opts )
+const o = f => g => h => f( g( h ) )
+
+const SumType = createSumTypeFactory( opts )
+
+describe( 'Array'
+        , () =>
+            describe( '#indexOf()'
+                    , () =>
+                        it( 'returns -1 when the value is not present'
+                          , () =>
+                              assert.equal( -1, [ 1, 2, 3 ].indexOf( 4 ) )
+                        )
+            )
+)
+
+describe( 'createSumTypeFactory'
+        , () =>
+            describe( '()'
+                    , () =>
+                        it( 'returns a function'
+                          , () =>
+                              assert( R.is( Function, createSumTypeFactory( opts ) ) )
+                          )
+                    )
+        )
+
 //region Definitions
 //    Point :: Type
 const Point =
@@ -18,38 +55,271 @@ const move2 =
           , shape
           )
 
-//    Shape :: Type
-const Shape =
-  SumType( 'Shape'
-         , 2
-         , 'url'
-         , [ { tag: 'Circle'
-             , type: $.RecordType( { origin: Point, radius: $.ValidNumber } )
-             , fns: { area: ( { radius } ) => Math.PI * radius * radius
-                    , move
-                    , move2
-                    }
-             }
-           , { tag: 'Square'
-             , type: $.RecordType( { origin: Point, side: $.ValidNumber } )
-             , fns: { area: ( { side } ) => side * side
-                    , move
-                    , move2
-                    }
-             }
-           , { tag: 'Rectangle'
-             , type: $.RecordType( { origin: Point, sides: Point } )
-             , fns: { area: ( { sides : [ w, h ] } ) => w * h
-                    , move
-                    , move2
-                    }
-             }
-           ]
-         , { move:  ts => [ Point, ts.PT, ts.PT ]
-           , move2: ts => [ $.ValidNumber, $.ValidNumber, ts.PT, ts.PT ]
-           , area:  ts => [ ts.PT, $.ValidNumber ]
+const shapeDef =
+  [ { tag: 'Circle'
+    , type: $.RecordType( { origin: Point, radius: $.ValidNumber } )
+    , fns: { area: ( { radius } ) => Math.PI * radius * radius
+           , move
+           , move2
            }
-         )
+    }
+  , { tag: 'Rect'
+    , type: $.RecordType( { origin: Point, sides: Point } )
+    , fns: { area: ( { sides : [ w, h ] } ) => w * h
+           , move
+           , move2
+           }
+    }
+  , { tag: 'Square'
+    , type: $.RecordType( { origin: Point, sides: Point } )
+    , fns: { area: ( { sides : [ w, h ] } ) => w * h
+           , move
+           , move2
+           }
+    }
+  ]
+
+const shapeSigs =
+  { move:  ts => [ Point, ts.PT, ts.PT ]
+  , move2: ts => [ $.ValidNumber, $.ValidNumber, ts.PT, ts.PT ]
+  , area:  ts => [ ts.PT, $.ValidNumber ]
+  }
+
+const shapeName = 'Shape'
+const shapeVersion = 2
+const shapeUrl = 'url'
+const Shape = SumType( shapeName, shapeVersion, shapeUrl, shapeDef, shapeSigs )
+
+describe( 'SumType', () =>
+  describe( '()', () =>
+    it( 'returns an object', () =>
+      assert( R.is( Object, SumType( 'Shape', 2, 'url', shapeDef, shapeSigs ) ) )
+    )
+  )
+)
+
+const cBare = { origin: [ 0, 0 ], radius: 5 }
+const cWrapped = Shape.Circle( cBare )
+
+const rBare = { origin: [ 0, 0 ], sides: [ 4, 5 ] }
+const rWrapped = Shape.Rect( rBare )
+
+const sBare = { origin: [ 0, 0 ], sides: [ 5, 5 ] }
+const sWrapped = Shape.Square( sBare )
+
+describe( 'Shape', () => {
+  describe( 'Constructors', () =>
+    {
+      describe( '#Shape()', () =>
+        {
+          describe( 'Wrapped Input', () =>
+            {
+              it( 'returns a Shape.ShapeType with member', () =>
+                assert( $.test( [], Shape.ShapeType, Shape.Shape( cWrapped ) ) )
+              )
+              it( 'returns a Shape.CircleType with circle', () =>
+                assert( $.test( [], Shape.CircleType, Shape.Shape( cWrapped ) ) )
+              )
+              it( 'return.value R.equals circle.value', () =>
+                assert( R.equals( Shape.Shape( cWrapped ).value, cWrapped.value ) )
+              )
+              it( 'return.value !== circle.value', () =>
+                assert( Shape.Shape( cWrapped ).value !== cWrapped.value )
+              )
+              describe( 'Ambiguous Type', () =>
+                {
+                  it( 'return has the first matching type (RectType) with square', () =>
+                    assert( $.test( [], Shape.RectType, Shape.Shape( sWrapped ) ) )
+                  )
+                }
+              )
+            }
+          )
+          describe( 'Bare Member Input', () =>
+            {
+              it( 'returns a Shape.ShapeType with member', () =>
+                assert( $.test( [], Shape.ShapeType, Shape.Shape( cBare ) ) )
+              )
+              it( 'returns a Shape.CircleType with circle', () =>
+                assert( $.test( [], Shape.CircleType, Shape.Shape( cBare ) ) )
+              )
+              it( 'return.value R.equals circle', () =>
+                assert( R.equals( Shape.Shape( cBare ).value, cBare ) )
+              )
+              it( 'return.value !== circle', () =>
+                assert( Shape.Shape( cBare ).value !== cBare )
+              )
+              describe( 'Ambiguous Type', () =>
+                {
+                  it( 'return has the first matching type (RectType) with square', () =>
+                    assert( $.test( [], Shape.RectType, Shape.Shape( sBare ) ) )
+                  )
+                }
+              )
+            }
+          )
+          describe( 'Non Member', () =>
+            {
+              it( 'throws a TypeError with a non member input', () =>
+                assert.throws( () => Shape.Shape( {} ), TypeError )
+              )
+            }
+          )
+        }
+      )
+      describe( '#Circle()', () =>
+        {
+          describe( 'Wrapped Input', () =>
+            {
+              it( 'returns a Shape.CircleType with member', () =>
+                assert( $.test( [], Shape.CircleType, Shape.Circle( cWrapped ) ) )
+              )
+              it( 'return.value R.equals circle.value', () =>
+                assert( R.equals( Shape.Circle( cWrapped ).value, cWrapped.value ) )
+              )
+              it( 'return.value !== circle.value', () =>
+                assert( Shape.Circle( cWrapped ).value !== cWrapped.value )
+              )
+              it( 'throws a TypeError with a sum type member that doesn\'t match', () =>
+                assert.throws( () => Shape.Circle( rWrapped ), TypeError )
+              )
+            }
+          )
+          describe( 'Bare Member Input', () =>
+            {
+              it( 'returns a Shape.CircleType with member', () =>
+                assert( $.test( [], Shape.CircleType, Shape.Circle( cBare ) ) )
+              )
+              it( 'return.value R.equals circle.value', () =>
+                assert( R.equals( Shape.Circle( cBare ).value, cBare ) )
+              )
+              it( 'return.value !== circle.value', () =>
+                assert( Shape.Circle( cBare ).value !== cBare )
+              )
+              it( 'throws a TypeError with a sum type member that doesn\'t match', () =>
+                assert.throws( () => Shape.Circle( rBare ), TypeError )
+              )
+            }
+          )
+          describe( 'Non Member', () =>
+            {
+              it( 'throws a TypeError with a non member input', () =>
+                assert.throws( () => Shape.Circle( {} ), TypeError )
+              )
+            }
+          )
+        }
+      )
+    }
+  )
+  describe( 'Types', () =>
+    {
+      describe( '#ShapeType', () =>
+        {
+          describe( 'Wrapped Input', () =>
+            {
+              it( 'matches a circle', () =>
+                assert( $.test( [], Shape.ShapeType, cWrapped ) )
+              )
+              it( 'matches a rect', () =>
+                assert( $.test( [], Shape.ShapeType, rWrapped ) )
+              )
+            }
+          )
+          describe( 'Bare Member Input', () =>
+            {
+              it( 'matches a circle', () =>
+                assert( $.test( [], Shape.ShapeType, cBare ) )
+              )
+              it( 'matches a rect', () =>
+                assert( $.test( [], Shape.ShapeType, rBare ) )
+              )
+            }
+          )
+          describe( 'Non Member', () =>
+            {
+              it( 'does not match non member', () =>
+                assert( R.not( $.test( [], Shape.ShapeType, {} ) ) )
+              )
+            }
+          )
+        }
+      )
+      describe( '#CircleType', () =>
+        {
+          describe( 'Wrapped Input', () =>
+            {
+              it( 'matches a circle', () =>
+                assert( $.test( [], Shape.CircleType, cWrapped ) )
+              )
+              it( 'does not match a rect', () =>
+                assert( R.not( $.test( [], Shape.CircleType, rWrapped ) ) )
+              )
+            }
+          )
+          describe( 'Bare Member Input', () =>
+            {
+              it( 'matches a circle', () =>
+                assert( $.test( [], Shape.CircleType, cBare ) )
+              )
+              it( 'does not match a rect', () =>
+                assert( R.not( $.test( [], Shape.CircleType, rBare ) ) )
+              )
+            }
+          )
+          describe( 'Non Member', () =>
+            {
+              it( 'does not match a non member', () =>
+                assert( R.not( $.test( [], Shape.RectType, {} ) ) )
+              )
+            }
+          )
+        }
+      )
+      describe( '#RectType', () =>
+        {
+          describe( 'Wrapped Input', () =>
+            {
+              it( 'matches a rect', () =>
+                assert( $.test( [], Shape.RectType, rWrapped ) )
+              )
+              it( 'does not match a circle', () =>
+                assert( R.not( $.test( [], Shape.RectType, cWrapped ) ) )
+              )
+            }
+          )
+          describe( 'Bare Member Input', () =>
+            {
+              it( 'matches a rect', () =>
+                assert( $.test( [], Shape.RectType, rBare ) )
+              )
+              it( 'does not match a circle', () =>
+                assert( R.not( $.test( [], Shape.RectType, cBare ) ) )
+              )
+            }
+          )
+          describe( 'Ambiguous Type', () =>
+            {
+              it( 'matches a square', () =>
+                assert( $.test( [], Shape.RectType, sBare ) )
+              )
+              it( 'matches a square', () =>
+                assert( $.test( [], Shape.RectType, sWrapped ) )
+              )
+            }
+          )
+          describe( 'Non Member', () =>
+            {
+              it( 'does not match a non member', () =>
+                assert( R.not( $.test( [], Shape.RectType, {} ) ) )
+              )
+            }
+          )
+        }
+      )
+    }
+  )
+})
 
 const Offer =
   SumType( 'Offer'
@@ -97,169 +367,4 @@ const Deal =
            ]
          , {}
          )
-
-const Bool =
-  UntaggedSumType( 'Bool'
-                 , 'url'
-                 , [ true, false ]
-                 )
-
-const IntegerOrString =
-  UntaggedSumType( 'IntegerOrString'
-                 , 'url'
-                 , [ $.Integer, $.String ]
-                 )
-
-const RegexFlags =
-  UntaggedSumType( 'RegexFlags'
-                 , 'url'
-                 , [ $.EnumType( 'Standard', 'url', [ 'g', 'i', 'm' ] )
-                   , $.EnumType( 'Experimental', 'url', [ 'u', 'y' ] )
-                   ]
-                 )
 //endregion
-
-// region Checks
-const ShapeChecks =
-  () =>
-  (
-    { z: ','
-    , '0 Shape': Shape
-    , '1 Inf Circle': Shape.Shape( { origin: [ 1, 2 ], radius: 2 } )
-    , '2 Inf Square': Shape.Shape( { origin: [ 1, 2 ], side: 4 } )
-    , '3 Inf Rectangle': Shape.Shape( { origin: [ 0, 0 ], sides: [ 5, 5 ] } )
-    //, '3 Inf NoMatch_1': Shape.Shape( [ [ 1, 2 ] ] )
-    //, '4 Inf NoMatch_2': Shape.Shape( [ [ 1, 2 ], [ true ] ] )
-    }
-  )
-
-const dealIdentity =
-  def( 'dealIdentity', {}, [ Deal.DealType, Deal.DealType ], x => x )
-const DealChecks =
-  () =>
-  (
-    { z: ','
-    , '0 Inf DealNeverSaved': Deal.Deal( { id: null } )
-    , '1 Inf DealSaved': Deal.Deal( { id: 1234 } )
-    // , '2 Inf DealNoMatch': Deal.Deal( { id: 'a' } )
-    , '3 DealNeverSaved': Deal.NeverSaved( { id: null } )
-    , '4 DealSaved': Deal.Saved( { id: 1234 } )
-    //, '5 DealNoMatch': Deal.Saved( { id: 'a' } )
-    , '6 dealIdentity NeverSaved bare': dealIdentity( { id: null } )
-    , '7 dealIdentity Saved bare': dealIdentity( { id: 1234 } )
-    // , '8 dealIdentity Reg bare': dealIdentity( { id: 'a' } ) // this breaks (as it should)
-    , '9 dealIdentity NeverSaved constr': dealIdentity( Deal.Deal( { id: null } ) )
-    , '9_ dealIdentity Reg constr': dealIdentity( Deal.Deal( { id: 1234 } ) )
-    }
-  )
-
-const BoolChecks =
-  () =>
-  (
-    { '0 BoolTrue': Bool( true )
-    , '1 BoolFalse': Bool( false )
-    , '2 BoolNoMatch': Bool( 1 )
-    }
-  )
-
-const IntegerOrStringChecks =
-  () =>
-  (
-    { IntegerOrStringInt: IntegerOrString( 1 )
-    , IntegerOrStringString: IntegerOrString( 'a string' )
-    , IntegerOrStringBoolean: IntegerOrString( true )
-    }
-  )
-
-const RegexFlagsChecks =
-  () =>
-  (
-    { G: RegexFlags( 'g' )
-    , Y: RegexFlags( 'y' )
-    , Z: RegexFlags( 'z' )
-    }
-  )
-//endregion
-
-
-if ( typeof window !== 'undefined' ) {
-  window.$ = $
-  window.S = S
-  window.type = type
-  window.def = def
-  window.Shape = Shape
-  window.circleDef = { origin: [ 1, 2 ], radius: 2 }
-  window.circle = Shape.Shape( window.circleDef )
-  window.squareDef = { origin: [ 1, 2 ], side: 4 }
-  window.square = Shape.Shape( window.squareDef )
-  window.rectDef = { origin: [ 0, 0 ], sides: [ 5, 5 ] }
-  window.rect = Shape.Shape( window.rectDef )
-  window.Offer = Offer
-  window.Deal = Deal
-  window.dealNeverSaved = Deal.Deal( { id: null } )
-}
-
-console.group( 'union-types' )
-console.log( 'ShapeChecks', ShapeChecks() )
-console.log( 'DealChecks', DealChecks() )
-////console.log( 'DealNew_tag', Deal.Deal( { id: null } ).tag )
-//console.log( 'BoolChecks', BoolChecks() )
-//console.log( 'IntegerOrStringChecks', IntegerOrStringChecks() )
-//console.log( 'RegexFlagsChecks', RegexFlagsChecks() )
-////console.log( 'validate', Union.validate( Shape.inferShape( 1, 2, 3 ) ) )
-////console.log( 'Right', S.Right(1) )
-////console.log( 'Real validate', $.Integer.validate( 1 ) )
-console.groupEnd()
-
-/*
-// times are with type-checking enabled / disabled
-console.time( 'circle.hasTags()' )
-R.times( _ => circle.hasTags( ['Circle'] ), 1000 ) // 12ms / 12ms
-console.timeEnd( 'circle.hasTags()' )
-console.time( 'Shape.hasTags(circle)' )
-R.times( _ => Shape.hasTags( ['Circle'], circle ), 1000 ) // 1.6ms / 1.5ms
-console.timeEnd( 'Shape.hasTags(circle)' )
-console.time( 'Shape.hasTags(circleDef)' )
-R.times( _ => Shape.hasTags( ['Circle'], circleDef ), 1000 ) // 91ms / 90ms
-console.timeEnd( 'Shape.hasTags(circleDef)' )
-
-console.time( 'circle.move2()' )
-R.times( _ => window.circle.move2( 1, 2 ) , 1000 ) // 8.5ms / 8.3ms
-console.timeEnd( 'circle.move2()' )
-console.time( 'circle.move2()()' )
-R.times( _ => window.circle.move2( 1 )( 2 ) , 1000 ) // 9.5ms / 6.3ms
-console.timeEnd( 'circle.move2()()' )
-console.time( 'circle.move' )
-R.times( _ => window.circle.move( [1,2] ), 1000 ) // 17ms / 12.7ms
-console.timeEnd( 'circle.move' )
-
-console.time( 'Shape.move2(circle)' )
-R.times( _ => Shape.move2( 1, 2, window.circle ) , 1000 ) // 171ms / 23ms
-console.timeEnd( 'Shape.move2(circle)' )
-console.time( 'Shape.move2(circleDef)' )
-R.times( _ => Shape.move2( 1, 2, window.circleDef ) , 1000 ) // 174ms / 100ms
-console.timeEnd( 'Shape.move2(circleDef)' )
-console.time( 'Shape.move2()()(circleDef)' )
-R.times( _ => Shape.move2( 1 )( 2 )( window.circleDef ) , 1000 ) // 153ms / 95ms
-console.timeEnd( 'Shape.move2()()(circleDef)' )
-console.time( 'Shape.move(circle)' )
-R.times( _ => Shape.move( [ 1, 2], window.circle ) , 1000 ) // 104ms / 21ms
-console.timeEnd( 'Shape.move(circle)' )
-console.time( 'Shape.move(circleDef)' )
-R.times( _ => Shape.move( [ 1, 2], window.circleDef ) , 1000 ) // 164ms / 58ms
-console.timeEnd( 'Shape.move(circleDef)' )
-*/
-
-/*
-R.identity( { z: ','
-            , '00 ShapeChecks': ShapeChecks()
-            , '10 DealChecks': DealChecks()
-            //, '20 DealNew_tag': Deal.Deal( { id: null } ).tag
-            , '30 BoolChecks': BoolChecks()
-            , '50 IntegerOrStringChecks': IntegerOrStringChecks()
-            , '60 RegexFlagsChecks': RegexFlagsChecks()
-            //, '80 validate': Union.validate( Shape.inferShape( 1, 2, 3 ) )
-            //, '90 Right': S.Right(1)
-            }
-          )
-*/
