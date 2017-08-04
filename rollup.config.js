6377ea0c0353ecfcca6e71e6ae76c250beedc3d8
Kurt Milam <kurt.milam@xioup.com>
2017-08-04 18:01:47 +0200

    Update rollup and eslint config to get packaging working

--- rollup.config.js
+++ rollup.config.js
@@ -3,9 +3,44 @@ import builtins    from "rollup-plugin-node-builtins"
 import commonjs    from "rollup-plugin-commonjs"
 import nodeResolve from "rollup-plugin-node-resolve"
 import replace     from "rollup-plugin-replace"
+import bundleSize  from "rollup-plugin-bundle-size"
 import uglify      from "rollup-plugin-uglify"
 
 export default {
+  moduleName: 'createSumTypeFactory' ,
+  globals: {
+    "sanctuary-def": "$",
+    "ramda/src/__": "__",
+    "ramda/src/ap": "ap",
+    "ramda/src/append": "append",
+    "ramda/src/clone": "clone",
+    "ramda/src/concat": "concat",
+    "ramda/src/contains": "contains",
+    "ramda/src/converge": "converge",
+    "ramda/src/curryN": "curryN",
+    "ramda/src/equals": "equals",
+    "ramda/src/findLastIndex": "findLastIndex",
+    "ramda/src/find": "find",
+    "ramda/src/flatten": "flatten",
+    "ramda/src/fromPairs": "fromPairs",
+    "ramda/src/init": "init",
+    "ramda/src/is": "is",
+    "ramda/src/last": "last",
+    "ramda/src/lensIndex": "lensIndex",
+    "ramda/src/memoize": "memoize",
+    "ramda/src/over": "over",
+    "ramda/src/path": "path",
+    "ramda/src/pluck": "pluck",
+    "ramda/src/reduce": "reduce",
+    "ramda/src/reduced": "reduced",
+    "ramda/src/repeat": "repeat",
+    "ramda/src/tap": "tap",
+    "ramda/src/toPairs": "toPairs",
+    "ramda/src/uniq": "uniq",
+    "ramda/src/values": "values",
+    "ramda/src/zipObj": "zipObj",
+
+  },
   plugins: [
     replace( {
       "global.setTimeout": "window.setTimeout",
@@ -28,11 +63,12 @@ export default {
         ]
       }
     } ),
-    babel( { exclude: "node_modules/**" } ),
+    babel( { exclude: [ "node_modules/**", "asdfasdf/asdf**" ] } ),
     process.env.NODE_ENV === "production" && uglify({
       compress: {
         passes: 3
       }
-    })
+    }),
+    bundleSize()
   ].filter(x => x)
  }
